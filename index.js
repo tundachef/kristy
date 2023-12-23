37bcdeb8f6f851761c28c0c67681c7b0fd340b92
Ray Zion <59836483+tundachef@users.noreply.github.com>
2023-12-23 21:14:05 +0300

    working for bnb

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const Web3 = require('web3');
+const { Web3 } = require('web3');
+const axios = require('axios');
 
 // Connect to your BSC node
-const web3 = new Web3('http://localhost:8545');
+const web3 = new Web3('http://127.0.0.1:7545');
 
 // Set the starting and ending block numbers
 const startBlock = 0; // Replace with your desired start block
@@ -10,50 +11,83 @@ const endBlock = 'latest';
 // Minimum balance threshold in USD
 const minBalanceThresholdUSD = 10;
 
-// Get the latest block number
-web3.eth.getBlockNumber().then(async (latestBlockNumber) => {
+async function getContractBalances(contractAddress) {
+  const balances = {
+    contract: contractAddress,
+    tokens: {},
+  };
+
+  // Check the overall balance of the contract
+  const overallBalanceInWei = await web3.eth.getBalance(contractAddress);
+  const overallBalanceInEth = web3.utils.fromWei(overallBalanceInWei, 'ether');
+
+  // Convert overall balance to USD (Assuming the first token is the main currency)
+  const overallBalanceInUSD = overallBalanceInEth * tokens[0].exchangeRate;
+
+  // Check if the overall balance is less than 10 USD
+  if (overallBalanceInUSD < minBalanceThresholdUSD) {
+    return null;
+  }
+
+  balances.overallBalance = overallBalanceInUSD;
+
+  return balances;
+}
+
+async function processBlocks() {
+  // Get the latest block number
+  const latestBlockNumber = await web3.eth.getBlockNumber();
   console.log(`Latest Block Number: ${latestBlockNumber}`);
 
-  // Determine the end block if it's set to 'latest'
-  const finalBlock = endBlock === 'latest' ? latestBlockNumber : Math.min(latestBlockNumber, endBlock);
+  const addressesArray = [];
 
   // Iterate through blocks in the specified range
-  for (let blockNumber = startBlock; blockNumber <= finalBlock; blockNumber++) {
+  for (let blockNumber = startBlock; blockNumber <= latestBlockNumber; blockNumber++) {
     try {
       // Get block details
       const block = await web3.eth.getBlock(blockNumber, true);
 
       if (block && block.transactions) {
+        const blockBalances = [];
+
         // Iterate through transactions in the block
         for (const tx of block.transactions) {
           // Check if it's a contract creation transaction
           if (!tx.to) {
             const contractAddress = tx.creates;
+            const contractBalances = await getContractBalances(contractAddress);
 
-            // Check the balance of the contract
-            const balanceInWei = await web3.eth.getBalance(contractAddress);
-            const balanceInEth = web3.utils.fromWei(balanceInWei, 'ether');
+            if (contractBalances) {
+              blockBalances.push(contractBalances);
 
-            console.log(`Contract Address: ${contractAddress}`);
-            console.log(`Balance in ETH: ${balanceInEth}`);
-
-            // Check balance in USD (Assuming 1 ETH = current ETH price in USD)
-            const ethToUsdExchangeRate = 2500; // Replace with the actual exchange rate
-            const balanceInUSD = balanceInEth * ethToUsdExchangeRate;
-
-            console.log(`Balance in USD: ${balanceInUSD}`);
-
-            // Check if the balance is greater than the threshold
-            if (balanceInUSD > minBalanceThresholdUSD) {
-              console.log(`Balance exceeds ${minBalanceThresholdUSD} USD. Additional processing logic can be added here.`);
-            } else {
-              console.log(`Balance is below ${minBalanceThresholdUSD} USD.`);
+              // Collect addresses with balances over 10 USD
+              addressesArray.push(contractAddress);
             }
           }
+        }
+
+        // Save addresses with balances over 10 USD to a JSON file
+        if (blockBalances.length > 0) {
+          const fileName = `${blockNumber}.json`;
+          fs.writeFileSync(fileName, JSON.stringify(blockBalances, null, 2));
+          console.log(`Addresses with Balances over ${minBalanceThresholdUSD} USD for Block ${blockNumber} saved to ${fileName}`);
         }
       }
     } catch (error) {
       console.error(`Error processing block ${blockNumber}:`, error);
     }
   }
-});
+
+  // Send addresses as an array to a link
+  try {
+    // const link = 'https://example.com/save-addresses';
+    // await axios.post(link, { addresses: addressesArray });
+    console.log(addressesArray);
+    console.log('Addresses sent to the link successfully.');
+  } catch (error) {
+    console.error('Error sending addresses to the link:', error);
+  }
+}
+
+// Start processing blocks
+processBlocks();
